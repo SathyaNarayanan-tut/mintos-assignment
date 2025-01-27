4f7c2f06c6de9aa0ea6a248b3295ccb6d489b006
SathyaNarayanan-tut <60778744+SathyaNarayanan-tut@users.noreply.github.com>
2025-01-27 11:53:01 +0200

    Created providers.tf

---
+++ providers.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.0.2"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.20.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = var.kubeconfig_path
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = var.kubeconfig_path
+  }
+}
